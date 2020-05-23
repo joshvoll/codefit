1512cfc4d4398c6414fc6da1a223bfa07a6964ed
Josue Rodriguez <joshvoll@yahoo.com>
2020-05-22 18:46:55 -0600

    fixin order

--- hackerrank/sock-merchant/recap.go
+++ hackerrank/sock-merchant/recap.go
@@ -1,10 +1,18 @@
 package hackerrank
 
+import "fmt"
+
 // SockMerchantRecap just practices
 func SockMerchantRecap(n int32, ar []int32) int32 {
 	res := int32(0)
 	for i := int32(0); i < n; i++ {
-	    for j := int32(0); j < n; j++ {
+		for j := int32(0); j < n-1; j++ {
+			if ar[j] > ar[j+1] {
+				ar[j], ar[j+1] = ar[j+1], ar[j]
+			}
+		}
+	}
+	fmt.Println(ar)
 
 	return res
 }
