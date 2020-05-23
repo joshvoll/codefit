9208102e0c31f1e3b6cf4de10458e34561d87d22
Josue Rodriguez <joshvoll@yahoo.com>
2020-05-22 18:43:55 -0600

    sockermerchant recap

---
+++ hackerrank/sock-merchant/recap.go
@@ -0,0 +1,10 @@
+package hackerrank
+
+// SockMerchantRecap just practices
+func SockMerchantRecap(n int32, ar []int32) int32 {
+	res := int32(0)
+	for i := int32(0); i < n; i++ {
+	    for j := int32(0); j < n; j++ {
+
+	return res
+}
--- hackerrank/sock-merchant/sockmerchant_test.go
+++ hackerrank/sock-merchant/sockmerchant_test.go
@@ -10,3 +10,12 @@ func TestSockMerchant(t *testing.T) {
 		t.Fatalf("want 3, got %v ", res)
 	}
 }
+
+func TestSockMerchantRecap(t *testing.T) {
+	tc := int32(9)
+	tt := []int32{10, 20, 20, 10, 10, 30, 50, 10, 20}
+	res := SockMerchantRecap(tc, tt)
+	if res != 3 {
+		t.Fatalf("want 3, got %v ", res)
+	}
+}
