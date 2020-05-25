314a7d1ef43bb89a56029aa5624e77d2102b4435
Josue Rodriguez <joshvoll@yahoo.com>
2020-05-25 08:33:14 -0600

    picking number solution

--- hackerrank/picking-number/pikingnumber.go
+++ hackerrank/picking-number/pikingnumber.go
@@ -1,5 +1,19 @@
 package hackerrank
 
 func pickingNumer(a []int32) int32 {
-	return 0
+	var mv = make(map[int32]int32)
+	for i := 0; i < len(a); i++ {
+		mv[a[i]]++
+	}
+	var max int32 = 0
+	for k, v := range mv {
+		if max < mv[k+1]+v {
+			max = mv[k+1] + v
+		}
+
+		if max < mv[k-1]+v {
+			max = mv[k-1] + v
+		}
+	}
+	return max
 }
